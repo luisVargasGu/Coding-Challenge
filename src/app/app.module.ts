d1409632523a5b359ec253a2cf228183df93c179
luisVargasGu <vargasluis012@gmail.com>
2019-04-04 21:10:05 -0400

    Added imports and decladations for cli generated components and services.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchDivComponent } from './search-div/search-div.component';
+import { PaymentComponent } from './payment/payment.component';
+import { GraphicsComponent } from './graphics/graphics.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchDivComponent,
+    PaymentComponent,
+    GraphicsComponent
   ],
   imports: [
     BrowserModule,
