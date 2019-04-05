16a25c6cdaf2663fc8a8e8148fb588dd29278e5c
luisVargasGu <vargasluis012@gmail.com>
2019-04-04 21:04:05 -0400

    Base component for the search and display of testnet address transaction history, and balance.

---
+++ src/app/search-div/search-div.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-div',
+  templateUrl: './search-div.component.html',
+  styleUrls: ['./search-div.component.sass']
+})
+export class SearchDivComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
