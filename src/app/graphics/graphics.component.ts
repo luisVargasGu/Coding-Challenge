7525baa62dad182c044ca016827f14a6d985f819
luisVargasGu <vargasluis012@gmail.com>
2019-04-08 20:34:17 -0400

    Had to change the styleUrls to point to the new scss file.

--- src/app/graphics/graphics.component.ts
+++ src/app/graphics/graphics.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-graphics',
   templateUrl: './graphics.component.html',
-  styleUrls: ['./graphics.component.sass']
+  styleUrls: ['./graphics.component.scss']
 })
 export class GraphicsComponent implements OnInit {
 
