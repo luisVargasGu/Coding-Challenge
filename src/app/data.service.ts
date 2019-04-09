cd276212bebbb238b67d9cdec84c55cfa423d4b9
luisVargasGu <vargasluis012@gmail.com>
2019-04-08 20:25:44 -0400

    Added the Loopup function for a Btcoin address.

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  // Look up the public address (or wallet/HD wallet name) you’re interested in querying
+  //  and retrieve the transaction history and account balance
+  getAddressInformation(address: string) {
+    // Url for transaction history api call
+    const url = 'https://api.blockcypher.com/v1/btc/main/addrs/' + address;
+    // get request
+    const req = this.http.get(url);
+    return req;
+  }
 }
