497c5e42a9ad22e56570c75212616e09524b2ca5
luisVargasGu <vargasluis012@gmail.com>
2019-04-08 20:27:24 -0400

    Had to change all the stlyUrl pointers to point to the correct scss files.

--- src/app/search-div/search-div.component.ts
+++ src/app/search-div/search-div.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/data.service';
 
 @Component({
   selector: 'app-search-div',
   templateUrl: './search-div.component.html',
-  styleUrls: ['./search-div.component.sass']
+  styleUrls: ['./search-div.component.scss']
 })
 export class SearchDivComponent implements OnInit {
+  // data from the API call
+  addrsData;
+  // data about a transaction input or output
+  txData;
+  // search bar attribute
+  private _address: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  // Get value of the input address
+  set address(value: string) {
+    this._address = value;
   }
 
+  // return address information
+  searchAddress() {
+    console.log(this._address);
+    this.data.getAddressInformation(this._address).subscribe(
+      data => {
+        this.addrsData = JSON.parse(JSON.stringify(data));
+        this.txData = this.addrsData.txrefs;
+        console.log(this.addrsData);
+        console.log(this.txData);
+      },
+      error => {
+        console.log(JSON.stringify(error.json()));
+      }
+    );
+  }
+
+  constructor(private data: DataService) {}
+
+  ngOnInit() {}
 }
