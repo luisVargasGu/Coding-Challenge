dd3005e1c6161dba6a43b33d00cf7f45ecd81a2e
luisVargasGu <vargasluis012@gmail.com>
2019-04-10 17:17:40 -0400

    Added libraries for bitcoinjs-lib, and started process of making a transaction.

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import * as bitcoin from 'bitcoinjs-lib';
 
-@Injectable({
-  providedIn: 'root'
-})
+
 export class DataService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private network: any) {
+    network = bitcoin.networks.testnet;
+  }
 
   // Look up the public address (or wallet/HD wallet name) you’re interested in querying
   //  and retrieve the transaction history and account balance
@@ -15,5 +16,22 @@ export class DataService {
     // get request
     const req = this.http.get(url);
     return req;
+  }
+
+  // Make transaction
+  makeTransaction() {
+    // Network specification
+    const test = this.network;
+    console.log(test);
+    // // Generating our testnet Keypair
+    // const keypair = bitcoin.ECPair.makeRandom({ network: testnet });
+    // // address
+    // const addr = bitcoin.payments.p2pkh({
+    //   pubkey: keypair.publicKey,
+    //   network: bitcoin.networks.testnet
+    // });
+    // // private key
+    // const pk = keypair.toWIF();
+    // console.log(addr, pk);
   }
 }
