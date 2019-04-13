31a1c146adca3ee26a69b1392a1713c593fda615
luisVargasGu <vargasluis012@gmail.com>
2019-04-12 22:29:03 -0400

    Created the setters for the input fields, and the makeTransaction() function. It still has errors, despite many attempts at trying to update their code to work with the new bitcoinjs-lib version.

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/data.service';
+
 
 @Component({
   selector: 'app-payment',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payment.component.scss']
 })
 export class PaymentComponent implements OnInit {
+  // sender addres
+  private _sender: string;
 
-  constructor() { }
+  // reciever addres
+  private _reciever: string;
+
+  // ammount to send
+  private _amount: number;
+
+  // Get value of the sender address
+  set sender(value: string) {
+    this._sender = value;
+  }
+
+  // Get value of the sender address
+  set reciever(value: string) {
+    this._reciever = value;
+  }
+
+  // Get value of the sender address
+  set amount(value: number) {
+    this._amount = value;
+  }
+
+  constructor(private data: DataService) { }
 
   ngOnInit() {
+  }
+
+  // make transaction
+  makeTransaction() {
+    this.data.makeTransaction(this._sender, this._reciever, this._amount);
   }
 
 }
